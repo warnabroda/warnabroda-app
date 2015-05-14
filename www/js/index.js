c482c3036afb376c72fd977bf5ed1445d1f50862
Alexandre Tavares <xptavares@gmail.com>
2015-05-14 01:08:38 -0300

    for dsing

--- www/js/index.js
+++ www/js/index.js
@@ -1,49 +1,32 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        //document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        //app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        //var parentElement = document.getElementById(id);
-        //var listeningElement = parentElement.querySelector('.listening');
-        //var receivedElement = parentElement.querySelector('.received');
 
-        //listeningElement.setAttribute('style', 'display:none;');
-        //receivedElement.setAttribute('style', 'display:block;');
+        initialize: function() {
+            $(document).off('pageshow').on('pageshow', '#main-page', this.bindEvents);
+            //$(document).delegate('#page_name', 'pageshow', function () {
+            //    var the_height = ($(window).height() - $(this).find('[data-role="header"]').height() - $(this).find('[data-role="footer"]').height());
+            //    $(this).height($(window).height()).find('[data-role="content"]').height(the_height);
+            //});
+        },
 
-        //console.log('Received Event: ' + id);
-    }
-};
+        hideAllDivs: function() {
+            $("#div_email").hide();
+        },
+
+        selectChange: function() {
+            $('#warn_by').change(function () {
+                var optionSelected = $(this).find("option:selected");
+                if( optionSelected.val() === "0" ) {
+                    $("#div_email").show();
+                    $("#div_tel").hide();
+                } else {
+                    $("#div_email").hide();
+                    $("#div_tel").show();
+                }
+            });
+        },
+
+        bindEvents: function() {
+           app.hideAllDivs();
+           app.selectChange();
+        }
+    };
