96c91ce999a6e51fe77784f27f57b15152317e0d
Alexandre Tavares <xptavares@gmail.com>
2015-05-18 01:29:02 -0300

    teste views

--- www/js/index.js
+++ www/js/index.js
@@ -29,17 +29,20 @@ function onError(contactError) {
 var app = {
 
   initialize: function() {
-      $(document).off('pageshow').on('pageshow', '#main-page', this.bindEvents);
-      //$(document).delegate('#page_name', 'pageshow', function () {
-      //    var the_height = ($(window).height() - $(this).find('[data-role="header"]').height() - $(this).find('[data-role="footer"]').height());
-      //    $(this).height($(window).height()).find('[data-role="content"]').height(the_height);
-      //});
+    height_content();
+    $(document).off('pageshow').on('pageshow', '#main-page', this.bindEvents);
+    //$(document).delegate('#page_name', 'pageshow', function () {
+    //    var the_height = ($(window).height() - $(this).find('[data-role="header"]').height() - $(this).find('[data-role="footer"]').height());
+    //    $(this).height($(window).height()).find('[data-role="content"]').height(the_height);
+    //});
 
-      // Wait for device API libraries to load
-      //
-      document.addEventListener("deviceready", onDeviceReady, false);
-      // device APIs are available
-      //
+    // Wait for device API libraries to load
+    //
+    document.addEventListener("deviceready", onDeviceReady, false);
+    // device APIs are available
+    //
+
+    forms();
   },
 
   hideAllDivs: function() {
@@ -63,3 +66,40 @@ var app = {
      app.selectChange();
   }
 };
+
+
+var forms = function() {
+  $( "#warnForm" ).submit(function( event ) {
+    // Stop form from submitting normally
+    event.preventDefault();
+   
+    // Get some values from elements on the page:
+    var $form = $( this ),
+      warn_val = $form.find( "#warn" ).val(),
+      email_val = $form.find( "#email" ).val(),
+      tel_val = $form.find( "#tel" ).val(),
+      warn_by_val = $form.find( '#select[name="warn_by"] option:selected' ).val(),
+
+      url = $form.attr( "action" );
+
+    $.ajax({
+      url: url, 
+      type: 'POST', 
+      contentType: 'application/json', 
+      data: JSON.stringify({warn: warn_val, email: email_val, tel: tel_val, warn_by: warn_by_val})    
+    });
+  });
+};
+
+
+var height_content = function() {
+  var screen = $.mobile.getScreenHeight();
+  var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
+  var footer = $(".ui-footer").hasClass("ui-footer-fixed") ? $(".ui-footer").outerHeight() - 1 : $(".ui-footer").outerHeight();
+  /* content div has padding of 1em = 16px (32px top+bottom). This step
+  can be skipped by subtracting 32px from content var directly. */
+  var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
+  var content = screen - header - footer - contentCurrent;
+  $(".ui-content").height(content);
+}
+
