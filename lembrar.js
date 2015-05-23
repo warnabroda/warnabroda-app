77c4ae4fac24daf6a675c0b23e5f37941c476bd4
Alexandre Tavares <xptavares@gmail.com>
2015-05-23 16:58:11 -0300

    fix index for home

---
+++ lembrar.js
@@ -0,0 +1,27 @@
+var pns = [
+{ type: 'work',   value: '212-555-1234', pref: false },
+{ type: 'mobile', value: '917-555-5432', pref: true },
+{ type: 'home',   value: '203-555-7890', pref: false }
+];
+var contacts = [
+ {id: 1, displayName: "teste",  phoneNumbers :pns },
+ {id: 2, displayName: "teste 2",  phoneNumbers :pns }
+];
+
+
+ractive_componentes.contacts(contacts);
+
+
+
+var ractive = new Ractive({
+  el: "#output_contacts",
+  template: '#template_li_contacts',
+  data: { contacts_list: contacts },
+  oncomplete: function () {
+    $('#con').listview();
+  }
+});
+
+ractive.on( 'activate', function ( event ) {
+  $( "#tel" ).val($(event.node).attr("tel"));
+});
