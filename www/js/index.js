4f74cb8bda023577e0ad6323c9d8bcea2fc42aaf
xptavares <xptavares@gmail.com>
2015-05-28 01:22:26 -0300

    teste

--- www/js/index.js
+++ www/js/index.js
@@ -35,7 +35,6 @@ var onError = function(contactError) {
 var app = {
 
   initialize: function() {
-    height_content();
     $(document).off('pageshow').on('pageshow', '#main-page', this.bindEvents);
     document.addEventListener("deviceready", onDeviceReady, false);
     document.addEventListener('deviceready', alertCarrier, false);
@@ -90,15 +89,3 @@ var forms = function() {
     });
   });
 };
-
-
-var height_content = function() {
-  var screen = $.mobile.getScreenHeight();
-  var header = $(".ui-header").hasClass("ui-header-fixed") ? $(".ui-header").outerHeight()  - 1 : $(".ui-header").outerHeight();
-  var footer = $(".ui-footer").hasClass("ui-footer-fixed") ? $(".ui-footer").outerHeight() - 1 : $(".ui-footer").outerHeight();
-  /* content div has padding of 1em = 16px (32px top+bottom). This step
-  can be skipped by subtracting 32px from content var directly. */
-  var contentCurrent = $(".ui-content").outerHeight() - $(".ui-content").height();
-  var content = screen - header - footer - contentCurrent;
-  $(".ui-content").height(content);
-}
