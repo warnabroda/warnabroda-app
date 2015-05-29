875b3ebc368bc6a1d0608193337dc4b515f8284b
Alexandre Tavares <xptavares@gmail.com>
2015-05-28 21:19:16 -0300

    teste

--- lembrar.js
+++ lembrar.js
@@ -3,10 +3,18 @@ var pns = [
 { type: 'mobile', value: '917-555-5432', pref: true },
 { type: 'home',   value: '203-555-7890', pref: false }
 ];
-var contacts = [
- {id: 1, displayName: "teste",  phoneNumbers :pns },
- {id: 2, displayName: "teste 2",  phoneNumbers :pns }
+var ems = [
+{ type: 'work',   value: '212-555-1234', pref: false },
+{ type: 'mobile', value: '917-555-5432', pref: true },
+{ type: 'home',   value: '203-555-7890', pref: false }
 ];
+var contacts = [
+ {id: 1, displayName: "teste",  phoneNumbers :pns, emails:  },
+ {id: 2, displayName: "teste 2",  phoneNumbers :pns, emails:  }
+];
+
+
+
 
 
 ractive_componentes.contacts(contacts);
