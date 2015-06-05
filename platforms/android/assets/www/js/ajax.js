bfc66354ebd30c32d1dfd289540f5be2ae05c4c7
Alexandre Tavares <xptavares@gmail.com>
2015-06-05 18:44:20 -0300

    fix ursl

--- platforms/android/assets/www/js/ajax.js
+++ platforms/android/assets/www/js/ajax.js
@@ -4,7 +4,7 @@ var Ajax = {
       $.getJSON(Env.url + "contact_types").done(function(data) {
         ractive_componentes.contact_types(data);
       }).fail(function() {
-        ractive_componentes.warn_bies(Env.default_vars.warn_bies);
+        ractive_componentes.contact_types(Env.default_vars.warn_bies);
         console.log( "error" );
       });
     },
@@ -27,14 +27,32 @@ var Ajax = {
           warn_val = $form.find( "#warn" ).val(),
           email_val = $form.find( "#email" ).val(),
           tel_val = $form.find( "#tel" ).val(),
-          warn_by_val = $form.find( '#select[name="warn_by"] option:selected' ).val();
+          warn_by_val = $form.find( '#select[name="warn_by"] option:selected' ).val(),
+          language = $( "input[name='language']:checked" ).val();
 
         $.ajax({
-          url:  Env.url + "warns.json",
+          url:  Env.url + "warnings",
           type: 'POST',
           contentType: 'application/json',
-          data: JSON.stringify({authenticity_token: "t2oO5K+SXC3PuPNX2hgXbwbP/4nATIJicWF1gvQEQks=", warn: warn_val, email: email_val, tel: tel_val, warn_by: warn_by_val})
+          data: JSON.stringify({browser: "Android",
+                                contact: tel_val ? email_val : tel_val,
+                                created_date: "2015-06-05T20:56:10.271Z",
+                                device: "Android",
+                                enableName: false,
+                                id_contact_type: warn_by_val,
+                                id_message: warn_val,
+                                ip: "187.94.99.90",
+                                lang_key: "pt-br",
+                                operating_system: "linux",
+                                raw: "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu Chromium/41.0.2272.76 Chrome/41.0.2272.76 Safari/537.36",
+                                timezone: "180",
+                                warning_resp: {}})
         });
+
+
+
+
+
       });
     }
   },
